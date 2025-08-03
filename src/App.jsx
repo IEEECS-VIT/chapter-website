c1f0cb043b7f02daef13f0b050b17a640a215828
anubhav-kayal <kayalanubhav14@gmail.com>
2025-08-03 11:49:06 +0530

    Fixed all the minor Bugs, Good to go for version 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import HeroSection from './homepage/HeroSection';
 import OurStory from './homepage/OurStory';
 import PreLoader from "./Preloader/Preloader";
-import Project from "./Project/Project";
-import Board from "./board_section/board";
+import Board from "./board_section/board"
+import EventsPage from "./eventsection/eventpage";
 import Gallery from "./gallery/FilmstripGallery";
 import Footer from "./footer/Contact";
-import EventsPage from "./eventsection/eventpage";
+
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -22,15 +22,12 @@ const App = () => {
   return (
     <div className="w-screen min-h-screen overflow-x-hidden">
       <HeroSection />
-      <Project />
-      <Board /> 
-      <div className="App">
+      <Board />
       <EventsPage />
-    </div>
-        <div className="min-h-screen bg-neutral-800 flex items-center justify-center">
+      <div className="min-h-screen bg-neutral-800 flex items-center justify-center">
         <Gallery />
       </div>
-      <Footer />  
+      <Footer />
     </div>
   );
 };
