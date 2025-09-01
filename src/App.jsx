e1239dd2e3ef1c7fbc67a450aa0f11e939d32cd6
d-e-v-14 <mehandrudev@gmail.com>
2025-09-01 10:55:27 +0530

    import path correct

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Events from "./events/Mobile";
 import EventsD from "./events/Desktop";
 import Gallery from "./gallery/FilmStrip";
 import Footer from "./footer/Contact";
-import PreLoader from "./preloader/PreLoader";
+import PreLoader from "./preLoader/PreLoader";
 import Project from "./project/Project";
 
 
