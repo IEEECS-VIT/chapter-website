4b7bc0a075bc8304555c82c9a30219dafc3d4a29
d-e-v-14 <mehandrudev@gmail.com>
2025-09-22 22:24:44 +0530

    added gpu acc

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,   
-    port: 5173,   
+    host: true,
+    port: 5173,
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('gsap')) return 'gsap';       
+            if (id.includes('react') || id.includes('react-dom')) return 'react-vendor'; 
+            return 'vendor';                                
+          }
+        },
+      },
+    },
+  },
+});
